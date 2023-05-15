059ebadb8470e430529028d13c49e21c9d0f4bd5
fabel134 <fabel134@gmail.com>
2023-05-15 14:23:18 -0600

    add a new directory with script for to run in cluster BetterLab

---
+++ scripts_metagenomics/betterlab/05AssiT_reads.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#Version 1
+#Script for to run an metagenomics analysis
+#bash *.sh 
+work_directory=$(pwd)
+
+echo "+:+:+:+:+:+:+:+:+:+:+:+:+:+:+:+:+:+:+:+:+:+"
+echo "||        Taxonomic Assignment           ||"
+echo "+:+:+:+:+:+:+:+:+:+:+:+:+:+:+:+:+:+:+:+:+:+"
+
+cd ${work_directory}/results/rawdata
+
+for infile in *_R1.fastq.gz
+do
+	base=$(basename ${infile} _R1.fastq.gz)
+	echo $base
+	echo "Taxonomic Assignment for reads"
+	mkdir ${work_directory}/results/asstax/reads
+	echo kraken2 --db /datos/metamex/krakendb --threads 8 \
+		--paired \
+	       	${work_directory}/results/trim/${base}_R1_val_1.fq.gz \
+		${work_directory}/results/trim/${base}_R2_val_2.fq.gz \
+		--output ${work_directory}/results/asstax/reads \
+		--report ${work_directory}/results/asstax/reads/${base}_reads.report
+
+	echo cut -f2,3 ${work_directory}/results/asstax/contigs/${base}_contigs.kraken \> ${work_directory}/results/asstax/contigs/${base}_contigs.krona.input
+	
+	#echo cut -f2,3 ${work_directory}/results/asstax/contigs/${base}_contigs.kraken > ${work_directory}/results/asstax/contigs/${base}_contigs.krona.input
+
+	echo "***ktImportTaxonomy***"
+	echo ktImportTaxonomy ${work_directory}/results/asstax/contigs/${base}_contigs.krona.input -o ${work_directory}/results/asstax/contigs/${base}_contigs.krona.out.html
+
+done
